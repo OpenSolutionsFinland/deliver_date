c58ea6d07aa664f755eb85ee6cc15877e748b8b5
Jesse Vartiainen <jezco@host-001.elisa>
2013-11-07 19:14:46 +0200

    delivery date to sale.order.line model

--- delivery_date.py
+++ delivery_date.py
@@ -1,11 +1,11 @@
 from osv import osv, fields
 
 class delivery_date_order(osv.osv):
-    _name='sale.order'
-    _inherit='sale.order'
+    _name='sale.order.line'
+    _inherit='sale.order.line'
     
-    columns = {
-        'delivery_date': fields.char('Delivery date'),
+    _columns = {
+        'delivery_date': fields.char('Deliver', size=64, required=False, translate=True),
     }
     
 delivery_date_order()
